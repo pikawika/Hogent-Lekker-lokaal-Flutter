1948cb819b43bd730e516f10707897346cd4b535
Bram De Coninck <bramdc@MacBook-Pro-van-Bram.local>
2018-06-19 17:21:00 +0200

    Url fix

--- lib/data/rest_data.dart
+++ lib/data/rest_data.dart
@@ -12,7 +12,7 @@ class RestData {
   factory RestData() => _instance;
 
   static const String BASE_URL =
-      "https://testlekkerlokaal.azurewebsites.net/api/mobieleapp/";
+      "https://lekkerlokaalbralenbre.azurewebsites.net/api/mobieleapp/";
 
   Future<Handelaar> meldHandelaarAan(String username, String password) async {
     final response = await http.get(BASE_URL + username + "/" + password);
